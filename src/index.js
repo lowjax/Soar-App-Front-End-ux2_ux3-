4b3fe7b371eec83b17f5dfa9ba9a03471a924dd3
Jack Cooke <jack.wheet.cooke@gmail.com>
2022-06-12 14:58:20 +1000

    public fodler reconfiguration

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-import { App } from "./app"
+import { App } from "./App"
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration"
 // import React from 'react';
 // import ReactDOM from 'react-dom';
@@ -12,7 +12,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 // const appElement = document.getElementById("app")
 // ReactDOM.render(<App />, appElement)
-const root = ReactDOM.createRoot(document.getElementById("root"))
+const root = ReactDOM.createRoot(document.getElementById("App"))
 
 root.render(
    <React.StrictMode>
