a78b0aedfcfda06a1216e434cd90fd0f44edefe7
Jack Cooke <jack.wheet.cooke@gmail.com>
2022-06-14 16:52:11 +1000

    console log on admincrud

--- src/components/AdminUserCRUD.jsx
+++ src/components/AdminUserCRUD.jsx
@@ -37,6 +37,7 @@ export default function UserCRUD() {
            .then((json) => {
               console.log(json)
               setUser(json)
+              console.log(user)
            })
            .catch(function (error) {
               // handle error
